d1df3921af0f0c0cd659e54259faac29487e0032
NandhiniHariharan <hariharannandi@gmail.com>
2024-05-20 07:31:05 -0700

    i have added the full program

--- Day1/Assignment1.java
+++ Day1/Assignment1.java
@@ -1,1 +1,16 @@
 //command to print stars
+import java.util.Scanner;
+public class Assignment1 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+         System.out.print("Enter the Integer: ");
+        int N=scanner.nextInt();
+        
+        for(int i=1;i<=N;i++){
+        for (int j=1;j<=i;j++) {
+            System.out.print(j);
+        }
+         System.out.println();
+  }
+}
+}
